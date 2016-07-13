ae942cd32764bf96fdeaa398d8241533cb09e03c
rafidShahriar <oporajeyo21@gmail.com>
2016-07-13 10:09:04 +0600

    My First Commit

---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Page | Online Blog</title>
+	<link rel="stylesheet" href="../style-admin.css">
+</head>
+<body>
+	<div id="wrapper-login">
+		<h1>Admin Login</h1>
+		<form action="" method="">
+			<table>
+				<tr>
+					<td>Username: </td>
+					<td><input type="text" name="username"></td>
+				</tr>
+				<tr>
+					<td>Password: </td>
+					<td><input type="password" name="password"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Login"></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Blog Design</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+	<div id="header">
+		<span class="logo">blog about web design</span>
+		<ul id="menu">
+			<li><a href="index.php"><img src="images/but1.gif" alt="" width="64" height="42" /></a></li>
+			<li><a href=""><img src="images/but2.gif" alt="" width="108" height="42" /></a></li>
+			<li><a href=""><img src="images/but3.gif" alt="" width="93" height="42" /></a></li>
+			<li><a href=""><img src="images/but4.gif" alt="" width="101" height="42" /></a></li>
+			<li><a href=""><img src="images/but5.gif" alt="" width="86" height="42" /></a></li>
+			<li><a href=""><img src="images/but6.gif" alt="" width="102" height="42" /></a></li>
+		</ul>
+		<img src="images/spacer.gif" alt="setalpm" width="120" height="120" border="0" usemap="#Map" class="rss" />
+        <map name="Map">
+          <area shape="circle" coords="60,60,63" href="#">
+      </map>
+	</div>
+	<div id="content">
+		<div id="posts">
+			<div class="post">
+				<h2>Retro Photos</h2>
+				<div><span class="date">Mar 18th</span><span class="categories">in: Photos, Retro</span></div>
+				<div class="description">
+					<p><img src="images/pic1.jpg" alt="" width="200" height="140" />
+					Lorem ipsum dolor sit amet, consectetuer adipi scing elit.Mauris urna urna, varius et, interdum a, tincidunt quis, libero. Aenean sit amturpis. Maecenas hendrerit, massa ac laoreet iaculipede mnisl ullamcorpermassa, cosectetuer feipsum eget pede. Proin nunc. Donec nonummy, tellus er sodales enim, in tincidunmauris in odio. Massa ac laoreet iaculipede nisl ullamcorpermassa, ac consectetuer feipsum eget pede.  Proin nunc. Donec massa. Nulla pulvinar, nisl ac convallis nonummy, tellus eros sodales enim, in tincidunt mauris in odio.  massa ac laoreet iaculipede niorpermassa,consectetuer feipsum eget pede. Proin nunc. Donec massa. Nulla pulvinar, nisl ac convallis nonummy, tellus eros sodales enim, in tincidunt mauris in odio. Lorem ipsum dolor sit amet, consectetuer adipi scing elit.Mauris urna urna, varius et, interdum a, tincidunt quis, libero. Aenean sit amturpis. </p>
+				</div>
+				<p class="comments">Comments - 17   <span>|</span>   <a href="index2.php">Continue Reading</a></p>
+			</div>
+			<div class="post">
+				<h2>Original Wordpress Themes</h2>
+				<div><span class="date">Mar 12th</span><span class="categories">in: Design, Themes</span></div>
+				<div class="description">
+					<p><img src="images/pic1.jpg" alt="" width="200" height="140" />Lorem ipsum dolor sit amet, consectetuer adipi scing elit.Mauris urna urna, varius et, interdum a, tincidunt quis, libero. Aenean sit amturpis. Maecenas hendrerit, massa ac laoreet iaculipede mnisl ullamcorpermassa, cosectetuer feipsum eget pede. Proin nunc. Donec nonummy, tellus er sodales enim, in tincidunmauris in odio. Massa ac laoreet iaculipede nisl ullamcorpermassa, ac consectetuer feipsum eget pede.  Proin nunc. Donec massa. Nulla pulvinar, nisl ac convallis nonummy, tellus eros sodales enim, in tincidunt mauris in odio.  massa ac laoreet iaculipede niorpermassa,consectetuer feipsum eget pede. Proin nunc. Donec massa. Nulla pulvinar, nisl ac convallis nonummy, tellus eros sodales enim, in tincidunt mauris in odio. Lorem ipsum dolor sit amet, consectetuer adipi scing elit.Mauris urna urna, varius et, interdum a, tincidunt quis, libero. Aenean sit amturpis. </p>
+				</div>
+				<p class="comments">Comments - 17   <span>|</span>   <a href="index2.php">Continue Reading</a></p>
+			</div>
+		</div>
+		<div id="sidebar">
+		    <div id="search">
+				<input type="text" value="Search"> <a href="#"><img src="images/go.gif" alt="" width="26" height="26" /></a>																																																																																																																																																																																																																																																						<div class="inner_copy"><a href="http://www.bestfreetemplates.info/flash.php">free flash templates</a><a href="http://www.beautifullife.info/web-design/15-best-free-website-builders/">best free web builders</a></div>
+			</div>
+			<div class="list">
+				<img src="images/title1.gif" alt="" width="186" height="36" />
+				<ul>
+					<li><a href="#">animation</a></li>
+					<li><a href="#">magazines</a></li>
+					<li><a href="#">architecture</a></li>
+					<li><a href="#">news</a></li>
+					<li><a href="#">art</a></li>
+					<li><a href="#">photography</a></li>
+					<li><a href="#">blogs</a></li>
+					<li><a href="#">product design</a></li>
+					<li><a href="#">books</a></li>
+					<li><a href="#">stuff</a></li>
+					<li><a href="#">graphic design</a></li>
+					<li><a href="#">web design</a></li>
+					<li><a href="#">illustration</a></li>
+				</ul>
+				<img src="images/title2.gif" alt="" width="180" height="34" />
+				<ul>
+					<li><a href="#">January</a></li>
+					<li><a href="#">July</a></li>
+					<li><a href="#">February</a></li>
+					<li><a href="#">August</a></li>
+					<li><a href="#">March</a></li>
+					<li><a href="#">September</a></li>
+					<li><a href="#">April</a></li>
+					<li><a href="#">October</a></li>
+					<li><a href="#">May</a></li>
+					<li><a href="#">November</a></li>
+					<li><a href="#">June</a></li>
+					<li><a href="#">December</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<p>Copyright &copy;. All rights reserved. Design by <a href="http://freecsstemplates.in">Free Css</a></p>																																																																		<div class="inner_copy"><a href="http://www.greatdirectories.org/offer.html">buy links with high pr</a><a href="http://www.bestfreetemplates.org/">free templates</a></div>
+	</div>
+</body>
+</html>
